e3e7d0fa1cd333d4b8295266079888f59d4a4b99
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2025-04-25 14:17:10 -0300

    FEAT: Criação da API para Modelo

--- app/urls.py
+++ app/urls.py
@@ -7,12 +7,13 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet, AcessorioViewSet, CorViewSet
+from core.views import UserViewSet, AcessorioViewSet, CorViewSet, ModeloViewSet
 
 router = DefaultRouter()
 
 router.register(r'acessorios', AcessorioViewSet, basename='acessorios')
 router.register(r'cores', CorViewSet, basename=  'cores')
+router.register(r'modelos', ModeloViewSet, basename='modelos')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
